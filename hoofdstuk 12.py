1723d22f67605b7cf0918ff15bbf421c1e837cf4
RowanPl <Rowanplooij@hotmail.com>
2024-05-15 11:25:27 +0200

    initialize

---
+++ hoofdstuk 12.py
@@ -0,0 +1,9 @@
+# Opgave 1:
+# Maak een tekstbestand met de naam, de geboortedatum en het telefoonnummer van je vrienden.
+
+# Opgave 2: Maak een programma waarbij je de naam van een vriend invoert en het programma de gegevens van die vriend
+# afdrukt met behulp van het tekstbestand dat je in opgave 1 hebt gemaakt.
+
+
+# Opgave 3:
+# Bepaal het gemiddelde cijfer per vak uit het bovengenoemde csv-bestand met behaalde cijfers.
